db050df8f4934964e21f50e244966a05bd801243
User404 <111229341+cat228608@users.noreply.github.com>
2023-12-20 14:01:57 +0300

    Update vk.py

--- vk.py
+++ vk.py
@@ -13,21 +13,21 @@ vk_session = vk_api.VkApi(token=ACCESS_TOKEN, captcha_handler=captcha_handler)
 
 vk = vk_session.get_api()
 
-user_id = '' #id вашего профиля вк
-
-wall_posts = vk.wall.get(owner_id=user_id, count=1)
-post_id = wall_posts['items'][0]['id']
-message = wall_posts['items'][0]['text']
+user_id = '492361343' #id вашего профиля вк
 
 while True:
-    target_date = datetime.datetime(2024, 6, 29) #дата дембеля или еще чего то
-    now = datetime.datetime.now()
-    delta = target_date - now
-    total_seconds = delta.total_seconds()
-    days = divmod(total_seconds, 86400)
-    hours = divmod(days[1], 3600)
-    minutes = divmod(hours[1], 60)
-    seconds = minutes[1]
-    text_msg = f"До даты {target_date} осталось: \nДней - {int(days[0])} дней, \nЧасов - {int(hours[0])} часов, \nМинут - {int(minutes[0])} минут, \nСекунд - {int(seconds)} секунд\n\nP.s Обновление таймера раз в 31 секунду потому вто инаве вк требует капчу"
-    vk.wall.edit(owner_id=user_id, post_id=post_id, message=text_msg)
-    time.sleep(31
+    try:
+    	target_date = datetime.datetime(2024, 6, 29) #дата дембеля или еще чего то
+    	now = datetime.datetime.now()
+    	delta = target_date - now
+    	total_seconds = delta.total_seconds()
+    	days = divmod(total_seconds, 86400)
+    	hours = divmod(days[1], 3600)
+    	minutes = divmod(hours[1], 60)
+    	seconds = minutes[1]
+    	text_msg = f"До даты ДМБ осталось: \n⌛{int(days[0])} дней, {int(hours[0])}ч : {int(minutes[0])}м : {int(seconds)}с\n\nUpdate: 31s"
+    	vk.status.set(text=text_msg)
+    	time.sleep(31)
+    except:
+    	time.sleep(31)
+    	continue
